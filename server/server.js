4e804aacee12812f9524e12107518171f8b70b4a
tkruba <tkruba@gmail.com>
2022-02-02 15:58:44 +0100

    Server port location

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.port || 3000;
+const port = process.env.port || process.env.SERVER_PORT;
 
 const server = http.createServer(app);
 
